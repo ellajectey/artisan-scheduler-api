c1ac5bf11345447df9370124d23df8f979565a14
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-04-15 07:20:29 +0000

    switched title to job  in the artisan model

--- src/models/artisan.js
+++ src/models/artisan.js
@@ -7,7 +7,7 @@ const ArtisanSchema = new schema({
 
     firstName: {type:String, required: true},
     lastName: {type:String, required:true},
-    title: {type:String, required:true},
+    job: {type:String, required:true},
     email: {type:String, required: true},
     password: {type:String, required: true},
     phoneNumber: {type:String, default: null},
