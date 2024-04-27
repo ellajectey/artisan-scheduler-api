4d094e9e2a6cda03598e578236c7da64d57fa534
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-04-27 20:05:58 +0000

    added some schema

--- src/models/appointment.js
+++ src/models/appointment.js
@@ -3,15 +3,14 @@ import mongoose from "mongoose";
 const schema = mongoose.Schema
 
 const AppointmentSchema = new schema({
-    
-
-   artisanId: {type:String, default: null, lowercase: true},
+    artisanId: {type:String, default: null, lowercase: true},
+    artisanName: {type:String, default:null},
+    email: {type:String, required: true},
     serviceType: {type:String, required:true},
+    description: {type:String, default:null},
     selectedDate: {type:Date, required: true},
     selectedTime: {type:String, required: true},
     depositAmount: {type:Number, required: true},
-
-
-
 })
+
 export const appointmentModel = mongoose.model("Appointment", AppointmentSchema)
